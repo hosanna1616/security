0ff88e889bef2eaf58bb63caa0f72a70dd9e96b2
hosanna1616 <hosannawalle@gmail.com>
2025-08-12 09:02:16 +0300

    Updated page.tsx and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import HeroSection from '@/components/HeroSection'
+import MainSection from '@/components/MainSection'
+import GashaWAFSection from "@/components/GashaWAFSection";
+
 
 export default function Gasha() {
   return (
     <>
       <HeroSection />
+      <MainSection />
+      <GashaWAFSection />
     </>
-  )
+  );
 }
