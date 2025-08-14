72c843b14145a5203abc8f64e69c8c011e42c915
hosanna1616 <hosannawalle@gmail.com>
2025-08-14 22:15:52 +0300

    adding signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,7 +6,17 @@ export default function SignUpPage() {
   return (
     <ClerkProvider>
       <div className="flex justify-center items-center h-screen">
-        <SignUp path="/signup" routing="path" signInUrl="/login" />
+        <SignUp
+          path="/signup"
+          routing="path"
+          redirectUrl="/dashboard" // 👈 After successful signup
+          appearance={{
+            elements: {
+              footer: { display: "none" },
+              headerSubtitle: { display: "none" },
+            },
+          }}
+        />
       </div>
     </ClerkProvider>
   );
