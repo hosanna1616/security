42b0df9a64b41ba5b12f7116f7929bc147f8fb49
Feven-Tolosa <tolosafeven0@gmail.com>
2025-08-28 14:17:51 +0300

    admin nave updated

--- components/AdminNavbar.tsx
+++ components/AdminNavbar.tsx
@@ -1,12 +1,13 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 // components/AdminNavbar.tsx
 export default function AdminNavbar() {
   return (
-    <header className='w-full bg-gray-900 border-b border-gray-700 px-6 py-4 flex justify-between items-center'>
+    <header className='w-full bg-gray-900 border-b border-gray-700 px-6 py-3 flex justify-between items-center'>
       {/* Logo / Title */}
-      <Link href='/Admin' className='text-xl font-bold text-primary'>
-        Admin Dashboard
+      <Link href='/'>
+        <Image src='/image/logo.png' alt='Logo' width={55} height={55} />
       </Link>
 
       {/* Profile / Settings */}
@@ -18,7 +19,7 @@ export default function AdminNavbar() {
           Settings
         </Link>
         <Link
-          href='/Admin/profile'
+          href='/Admin/settings'
           className='w-10 h-10 rounded-full bg-primary flex items-center justify-center'
         >
           <span className='font-bold'>A</span>
